658a47d08a15f503e5e745e7a3ad233b093dbc86
Diego <diego.scano77@gmail.com>
2021-01-08 16:05:19 +0100

    seconda bozza (sbagliata)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-function randomNumber(min, max) {
-  var result = Math.floor(Math.random() * (max + 1 - min) + min);
-  return result;
-}
-
 var bombNumbers = [];
 var num;
+var userNumbers;
+var pointCounter = 0;
 
 for (var x = 0; x < 16; x++) {
   num = randomNumber(1, 100);
   bombNumbers.push(num);
 }
 
+function randomNumber(min, max) {
+  var result = Math.floor(Math.random() * (max + 1 - min) + min);
+  return result;
+}
+
+function bombsCheck(list, number) {
+  var find = false;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] === number) {
+      find = true;
+    }
+  }
+  return find;
+}
+
 console.log(bombNumbers);
 
-var userNumbers;
-var pointCounter = 0;
-
-for (var y = 0; y < 84; y++) {
+var y;
+while (bombsCheck(bombNumbers, userNumbers) || y < 84) {
   userNumbers = prompt('Inserisci numero');
-  if (userNumbers[y] === bombNumbers[x]) {
+  if (bombsCheck(bombNumbers, userNumbers)) {
     alert('Hai beccato la bomba!');
   } else {
     pointCounter += 1;
   }
+  y++;
 }
 
 console.log(pointCounter);
