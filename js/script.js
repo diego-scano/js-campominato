6b254623119323be5a9b2af44f9f4f94b17df5f0
Diego <diego.scano77@gmail.com>
2021-01-08 17:40:40 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 var bombNumbers = [];
-var num;
 var userNumbers;
-var pointCounter = 0;
-
-for (var x = 0; x < 16; x++) {
-  num = randomNumber(1, 100);
-  bombNumbers.push(num);
-}
+var userArray = [];
 
 function randomNumber(min, max) {
   var result = Math.floor(Math.random() * (max + 1 - min) + min);
   return result;
+}
+
+while (bombNumbers.length < 16) {
+  var replyNum = randomNumber(1, 100);
+  if(!bombNumbers.includes(replyNum)) {
+    bombNumbers.push(replyNum)
+  }
+}
+
+function userNumberCheck(userChoice, min, max) {
+  var check = false;
+  if (userChoice < min || userChoice > max || isNaN(userChoice)) {
+    check = true;
+  }
+  return check;
 }
 
 function bombsCheck(list, number) {
@@ -25,15 +34,28 @@ function bombsCheck(list, number) {
 
 console.log(bombNumbers);
 
-var y;
-while (bombsCheck(bombNumbers, userNumbers) || y < 84) {
-  userNumbers = prompt('Inserisci numero');
-  if (bombsCheck(bombNumbers, userNumbers)) {
-    alert('Hai beccato la bomba!');
+var y = 0;
+
+while (!bombsCheck(bombNumbers, userNumbers) && y < 84) {
+  userNumbers = parseInt(prompt('Inserisci numero'));
+
+  if (userNumberCheck(userNumbers, 1, 100)) {
+    alert('Puoi inserire solo un numero tra 1 e 100');
+  } else if (userArray.includes(userNumbers)) {
+    alert('Non puoi inserire lo stesso numero');
   } else {
-    pointCounter += 1;
+    userArray.push(userNumbers);
+
+    console.log(userArray);
+    if (bombsCheck(bombNumbers, userNumbers)) {
+      alert('Hai beccato la bomba!');
+    } else {
+      if (userArray.length === 84) {
+        alert('Hai vinto!')
+      }
+    }
   }
   y++;
 }
 
-console.log(pointCounter);
+console.log('Il tuo punteggio è: ' + (userArray.length - 1));
